f2b5fae5c5e741e70ecdb5b500bd9bdfbe8ec88d
ArminKardan <armin.fire@gmail.com>
2025-04-30 13:53:25 +0330

    Update

--- run.sh
+++ run.sh
@@ -5,13 +5,12 @@ if [[ ! -d "/apps/main" ]] || [[ "$rebuild" == "true" ]]; then
 
     echo 'Hi! Welcome to QE Auto publisher!'
 
+    git clone -n --filter=tree:0 --sparse ${giturl} /apps/main
+    sleep 1
     cd /apps/main
-
-    git init
-    git remote add origin ${giturl}
-    git sparse-checkout init
-    git sparse-checkout set ${block}
-    git pull origin master
+    git sparse-checkout init --no-cone                        
+    git sparse-checkout set /${block}/
+    git checkout master
 
     cp -r /apps/linux/node /apps/main/${block}/node_modules
 
